769a41ace7107fd516548ac628dc78071d189da9
yushin <kkad45@naver.com>
2024-05-25 20:09:16 +0900

    fireBase연동

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {BrowserRouter as Router } from "react-router-dom"
+import firebaseApp from './firebaseApp';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+console.log(firebaseApp)
 root.render(
   <React.StrictMode>
     <Router>
