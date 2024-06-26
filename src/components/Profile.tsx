5f6d752d6b0cf086be97e20f22cfcfabe829f349
yushin <kkad45@naver.com>
2024-06-26 18:58:29 +0900

    useContext 구현

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,10 +1,12 @@
+import AuthContext from "context/Authenticate";
 import { getAuth, signOut } from "firebase/auth";
 import firebaseApp from "firebaseApp";
-import { Link, useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import {useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
 export default function Profile() {
-    const auth = getAuth(firebaseApp);
+    const {user} = useContext(AuthContext);
     const navigate = useNavigate();
 
     return (
@@ -12,12 +14,12 @@ export default function Profile() {
             <div className="flex__box-lg">
                 <div className="profile__image"></div>
                 <div>
-                    <div className="profile__email">{auth?.currentUser?.email}</div>
-                    <div className="profile__name">{auth?.currentUser?.displayName ? auth?.currentUser?.displayName : '사용자'}</div>
+                    <div className="profile__email">{user?.email}</div>
+                    <div className="profile__name">{user?.displayName ? user?.displayName : '사용자'}</div>
                 </div>
-            </div>
+            </div> 
             <div onClick={async () => {
-                getAuth(firebaseApp);
+                const auth = getAuth(firebaseApp);
                 await signOut(auth)
                 toast.success("로그아웃에 성공했습니다.")
                 navigate("/login");
---
+++ src/context/Authenticate.tsx
@@ -0,0 +1,36 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged, User } from 'firebase/auth'
+import firebaseApp from '../firebaseApp'
+
+
+interface AuthProps {
+    children: ReactNode;
+  }
+
+const AuthContext = createContext({
+    user: null as User | null,
+})
+
+export function AuthContextProvider({ children }: AuthProps) {
+    const auth = getAuth(firebaseApp);
+    const [currentUser, setCurrentUser] = useState<User | null>(null);
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setCurrentUser(user);
+            } else {
+                setCurrentUser(user);
+            }
+        });
+    }, [auth])
+
+
+    return (
+        <AuthContext.Provider value={{ user: currentUser }}>
+          {children}
+        </AuthContext.Provider>
+      );
+}
+
+export default AuthContext;
