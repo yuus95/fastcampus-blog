83043cf41feaaae83e73a3ffb9248595ea81d02d
yushin <kkad45@naver.com>
2024-06-27 08:55:07 +0900

    게시글 작성 일부 구현

--- src/components/PostForm.tsx
+++ src/components/PostForm.tsx
@@ -1,13 +1,32 @@
+import { ReactElement, ReactEventHandler, ReactHTMLElement, TextareaHTMLAttributes, useState } from "react"
+
 
 export default function PostForm() {
+    const [title, setTitle] = useState<string>("");
+    const [summary, setSummary] = useState<string>("");
+    const [content, setContent] = useState<string>("");
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {name,value} = e.target;
+        if (name ==='title'){
+            setTitle(value);
+        }
+
+        if (name ==='summary'){
+            setSummary(value);
+        }
+    }
+
     return (
         <>
-        <form action="/post" method="POST" className="form">
+            <form action="/post" method="POST" className="form">
                 <div className="form__block">
                     <label htmlFor="title">제목 </label>
                     <input type="text"
                         name="title"
                         id="title"
+                        onChange={onChange}
+                        value={title}
                         required />
                 </div>
                 <div className="form__block">
@@ -15,19 +34,22 @@ export default function PostForm() {
                     <input type="text"
                         name="summary"
                         id="summary"
+                        onChange={onChange}
+                        value={summary}
                         required />
                 </div>
                 <div className="form__block">
                     <label htmlFor="content">내용 </label>
-                    <textarea name="content"
-                        id="content"  />
+                    <textarea 
+                    name="content"
+                        id="content" />
                 </div>
 
                 <div className="form__block">
-                    <input type="button" className="form__btn--submit" value="제출"/>
+                    <input type="button" className="form__btn--submit" value="제출" />
                 </div>
             </form>
         </>
-        
+
     )
 }
